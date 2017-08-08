78ebaeb274c287a23f90137c404ebbed9cad4299
sylvain <sylvain.laugier.net@gmail.com>
2017-08-08 19:52:34 +0200

    intialized db

---
+++ imports/api/relationships.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo'
+
+export const Relationships = new Mongo.Collection('relationships');
---
+++ imports/ui/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() { 
+    return (
+      <div className="app-container">
+        <h1>Hello World </h1>
+      </div>
+    )
+  }
+
+}
