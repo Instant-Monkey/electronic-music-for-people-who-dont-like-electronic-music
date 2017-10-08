a98ecd8d806f41906b51221d2ac9c722df9eec6b
sylvain <sylvain.laugier.net@gmail.com>
2017-10-08 23:45:19 +0200

    added filtering for artists

--- imports/ui/components/admin/dashboards/ArtistsDashboard.js
+++ imports/ui/components/admin/dashboards/ArtistsDashboard.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import TextField from 'material-ui/TextField';
+
 import ArtistsList from '../artists/ArtistsList.js';
 
 export default class ArtistsDashboard extends Component {
-  constructor() {
-    super(props)
-    this.filterArtists = this.filterArtists.bind(this)
+  constructor(props) {
+    super(props);
+    this.filterArtists = this.filterArtists.bind(this);
+    this.state = {
+      filterFieldValue: '',
+      artistsToShow: this.props.artists,
+    };
+  }
+  filterArtists(e, value) {
+    const artistsToShow = this.props.artists.filter(artist =>
+      artist.artistInfo.artistName.toLowerCase().includes(value.toLowerCase()));
+    this.setState({
+      filterFieldValue: value,
+      artistsToShow,
+    });
+  }
+  render() {
+    return (
+      <div className="artist-dashboard-container">
+        <TextField
+          value={this.state.filterFieldValue}
+          hintText="filtrer les artistes"
+          onChange={this.filterArtists}
+        />
+        <ArtistsList
+          artists={this.state.artistsToShow}
+          albums={this.props.albums}
+        />
+      </div>
+    );
   }
 }
-  <ArtistsList
-    artists={props.artists}
-    albums={props.albums}
-  />
-);
 
 ArtistsDashboard.propTypes = {
   artists: PropTypes.arrayOf(PropTypes.object).isRequired,
